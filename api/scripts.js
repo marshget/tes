159a7dec13e974b6416e0c4fdb21a92bb49aedaa
marshget <marshbranded5@gmail.com>
2024-10-17 20:59:16 +0700

    Update scripts.js

--- api/scripts.js
+++ api/scripts.js
@@ -2,18 +2,13 @@ import { promises as fs } from 'fs';
 import path from 'path';
 
 export default async function handler(req, res) {
-    const filePath = path.join(process.cwd(), 'public', 'scripts', 'tes.lua'); // Arahkan ke file Lua
+    const filePath = path.join(process.cwd(), 'public', 'scripts', 'tes.lua'); // Mengarah ke file Lua
     try {
-        // Baca file Lua
         const luaScript = await fs.readFile(filePath, 'utf8');
-        
-        // Set header konten sebagai plain text
         res.setHeader('Content-Type', 'text/plain');
-        
-        // Kirim respons script Lua
         res.status(200).send(luaScript);
     } catch (error) {
-        // Jika ada error
+        console.error('Error reading Lua script:', error); // Log error di Vercel
         res.status(500).send('Error reading Lua script');
     }
 }
