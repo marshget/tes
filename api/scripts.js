d581d516a9642088744ee33e487fce535980675a
marshget <marshbranded5@gmail.com>
2024-10-17 21:29:16 +0700

    Update scripts.js

--- api/scripts.js
+++ api/scripts.js
@@ -6,6 +6,9 @@ export default async function handler(req, res) {
 
     try {
         const luaScript = await fs.readFile(filePath, 'utf8');
+        if (!luaScript) {
+            return res.status(404).send('Lua script not found');
+        }
         res.setHeader('Content-Type', 'text/plain');
         res.status(200).send(luaScript);
     } catch (error) {
